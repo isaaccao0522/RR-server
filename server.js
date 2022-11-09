2957e9f031f8c72c164b7e9e95814463d80d5a79
Derek Tsao <43665914+isaaccao0522@users.noreply.github.com>
2022-11-09 13:44:29 +0800

    Add: 'origin, credentials' to '.cors'.

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ mongoose
 
 const app = express ();
 
-app.use ( cors ());
+app.use ( cors ({
+  origin: '*',
+  credentials: true
+}));
+
 app.use ( express.json ());
 app.use ( express.urlencoded ({ extended: true }));
 
