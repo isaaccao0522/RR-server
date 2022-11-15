cdd19ced71e62bb5e69b8660dc123cff4a986f56
Derek Tsao <43665914+isaaccao0522@users.noreply.github.com>
2022-11-15 17:29:06 +0800

    Fix: remove unexpected token.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
-import express from ( 'express');
-import bcrypt from ( 'bcryptjs');
-import asyncHandler from ( 'express-async-handler');
-import User from ( '../models/userModel.js');
-import { isAuth, generateToken } from ('../utils.js');
+import express from  'express';
+import bcrypt from 'bcryptjs';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import { isAuth, generateToken } from '../utils.js';
 
 
 const userRouter = express.Router ();
