1cc163f0a5ecdc5a9d6ab34433041c5bbf9825fb
Derek Tsao <43665914+isaaccao0522@users.noreply.github.com>
2022-11-15 17:34:05 +0800

    Recovery: undo isAdmin.

--- utils.js
+++ utils.js
@@ -37,10 +37,10 @@ export const isAuth = ( req, res, next) => {
   }
 };
 
-// export const isAdmin = ( req, res, next) => {
-//   if ( req.user && req.user.isAdmin) {
-//     next ();
-//   } else {
-//     res.status ( 401).send ({ message: 'Invalid Admin Token' });
-//   }
-// };
+export const isAdmin = ( req, res, next) => {
+  if ( req.user && req.user.isAdmin) {
+    next ();
+  } else {
+    res.status ( 401).send ({ message: 'Invalid Admin Token' });
+  }
+};
